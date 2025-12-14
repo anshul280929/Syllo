bbf77751c3ab2358ddb4fb44435f92be130b0960
Anshul <anshulbhaskar50@gmail.com>
2025-12-15 01:44:11 +0530

    Initial commit

---
+++ syllo-backend/src/main/java/com/example/syllo_backend/SylloBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.syllo_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SylloBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SylloBackendApplication.class, args);
+	}
+
+}
