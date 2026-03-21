ff1cfb7a8fd25582074b954b288bc9c107b7685f
Anshul <anshulbhaskar50@gmail.com>
2026-03-22 00:59:08 +0530

    Subject controller and Service

--- syllo-backend/src/main/java/com/example/syllo_backend/Controller/SubjectController.java
+++ syllo-backend/src/main/java/com/example/syllo_backend/Controller/SubjectController.java
@@ -1,4 +1,30 @@
 package com.example.syllo_backend.Controller;
 
+import com.example.syllo_backend.Model.Subject;
+import com.example.syllo_backend.Services.SubjectService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/subjects")
+@RequiredArgsConstructor
 public class SubjectController {
+    private final SubjectService subjectService;
+
+    @PostMapping
+    public Subject create(@RequestParam String name,
+                          Authentication authentication) {
+        String email = authentication.getName();
+        return subjectService.createSubject(name, email);
+    }
+
+    @GetMapping
+    public List<Subject> list(Authentication authentication){
+        String email=authentication.getName();
+        return subjectService.getSubjects(email);
+    }
+
 }
--- syllo-backend/src/main/java/com/example/syllo_backend/Services/SubjectService.java
+++ syllo-backend/src/main/java/com/example/syllo_backend/Services/SubjectService.java
@@ -1,4 +1,40 @@
 package com.example.syllo_backend.Services;
 
+import com.example.syllo_backend.Model.Subject;
+import com.example.syllo_backend.Model.User;
+import com.example.syllo_backend.Repository.SubjectRepository;
+import com.example.syllo_backend.Repository.SubjectRepository;
+import com.example.syllo_backend.Repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class SubjectService {
+
+    private final SubjectRepository subjectRepository;
+    private final UserRepository userRepository;
+
+    public Subject createSubject(String subjectName, String userEmail) {
+
+        User user = userRepository.findByEmail(userEmail)
+                .orElseThrow();
+
+        Subject subject = Subject.builder()
+                .name(subjectName)
+                .user(user)
+                .build();
+
+        return subjectRepository.save(subject);
+    }
+
+    public List<Subject> getSubjects(String email) {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow();
+
+        return subjectRepository.findByUser(user);
+    }
 }
